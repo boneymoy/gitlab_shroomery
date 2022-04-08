73c8bcdc0cb1ed0795a33ad611c5c31e73c7d166
tim <tk@tower>
2022-04-08 09:50:41 +0200

    update gui

--- gui_plot.py
+++ gui_plot.py
@@ -28,14 +28,15 @@ app.layout = html.Div(
         'backgroundColor': colors['background']
     },
     children=[
-        html.H1(children='Hello Dash',
+        html.H1(children='Hello Ass',
                 style={
                     'textAlign': 'center',
-                    'color': colors['text']
+                    'color': colors['text'],
+                    'backgroundColor': colors['background']
                 }),
 
         html.Div(children='''
-            Dash: A web application framework for your data.
+            ShroomRoom
         ''',
                  style={
                      'textAlign': 'center',
@@ -64,29 +65,84 @@ def update_graph_live(n):
                     stderr=subprocess.STDOUT)
     df = pd.read_csv(SAVE_FILE_PATH)
     # fig.update_traces(overwrite=True)
+    # fig = make_subplots(rows=4, cols=2)
     fig = make_subplots(rows=2, cols=2)
     fig.add_trace(
-        go.Scatter(x=df['time'], y=df['hum']),
-        row=1, col=1
+        go.Scatter(name="CO2",
+                   x=df['time'],
+                   y=df['co2'],
+                   line=go.Line(color='#8ec07c',
+                                width=6),
+                   ),
+        row=1, col=1,
     )
     fig.add_trace(
-        go.Scatter(x=df['time'], y=df['rand']),
+        go.Scatter(name="Humidity",
+                   x=df['time'],
+                   y=df['humidity'],
+                   line=go.Line(color='#b16286',
+                                width=6)),
         row=1, col=2
     )
     fig.add_trace(
-        go.Scatter(x=df['time'], y=df['hum']),
+        go.Scatter(name="Temperature",
+                   x=df['time'],
+                   y=df['temperature'],
+                   line=go.Line(color='#d79921',
+                                width=6)),
         row=2, col=1
     )
+    """
     fig.add_trace(
-        go.Scatter(x=df['time'], y=df['rand']),
+        go.Scatter(x=df['time'][:50],
+                   y=df['time'][:50],
+                   line=go.Line(color='#458588',
+                                width=6)),
         row=2, col=2
     )
-
+    fig.add_trace(
+        go.Scatter(x=df['time'][:50],
+                   y=df['co2'][:50],
+                   line=go.Line(color='#355629',
+                                width=6),
+                   ),
+        row=3, col=1,
+    )
+    fig.add_trace(
+        go.Scatter(x=df['time'][:50],
+                   y=df['humidity'][:50],
+                   line=go.Line(color='#67324a',
+                                width=6)),
+        row=3, col=2
+    )
+    fig.add_trace(
+        go.Scatter(x=df['time'][:50],
+                   y=df['temperature'][:50],
+                   line=go.Line(color='#845f15',
+                                width=6)),
+        row=4, col=1
+    )
+    fig.add_trace(
+        go.Scatter(x=df['time'][:50],
+                   y=df['time'][:50],
+                   line=go.Line(color='#224244',
+                                width=6)),
+        row=4, col=2
+    )
+    """
     fig.update_layout(
         plot_bgcolor=colors['background'],
         paper_bgcolor=colors['background'],
-        font_color=colors['text']
+        font_color=colors['text'],
     )
+    fig.update_xaxes(showgrid=True,
+                     gridwidth=1,
+                     gridcolor='#928374',
+                     # tickvals=range(50, step=10),
+                     showticklabels=False)
+    fig.update_yaxes(showgrid=True,
+                     gridwidth=1,
+                     gridcolor='#928374')
     return fig
 
 
